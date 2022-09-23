6d9cc6c00dbbd4ee958acae6bea71f00312d27bf
circle-gon <circle-gon@users.noreply.github.com>
2022-09-23 16:33:00 +0000

    Minify source code

--- js/technical/loader.js
+++ js/technical/loader.js
@@ -1,8 +1,1 @@
-// Load files
-
-for (const file of modInfo.modFiles) {
-  const script = document.createElement("script");
-  script.setAttribute("src", `js/${file}`);
-  document.head.append(script);
-}
-
+for(const e of modInfo.modFiles){const t=document.createElement("script");t.setAttribute("src",`js/${e}`),document.head.append(t)}
